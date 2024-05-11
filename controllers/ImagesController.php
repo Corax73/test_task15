747f55a3c4fe540a4896e72b7c7308eb487bc24d
Corax73 <ramaz999@inbox.ru>
2024-05-11 22:13:22 +0400

    working with API

--- controllers/ImagesController.php
+++ controllers/ImagesController.php
@@ -3,8 +3,27 @@
 namespace app\controllers;
  
 use yii\rest\ActiveController;
+use yii\helpers\ArrayHelper;
  
 class ImagesController extends ActiveController
 {
     public $modelClass = 'app\models\Image';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['delete'], $actions['create'], $actions['update'], $actions['options']);
+        return ArrayHelper::merge($actions, [
+            'index' => [
+                'pagination' => [
+                    'pageSize' => 5,
+                ],
+                'sort' => [
+                    'defaultOrder' => [
+                        'created_at' => SORT_DESC,
+                    ],
+                ],
+            ],
+        ]);
+    }
 }
